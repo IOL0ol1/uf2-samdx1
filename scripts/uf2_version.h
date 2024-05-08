f17b954d2404cd894b5db7f5aba362e515292ff2
IOl0ol1 <IOL0ol1@outlook.com>
2024-05-08 16:42:48 +0800

    create microchip studio SAMD51 project file

---
+++ scripts/uf2_version.h
@@ -0,0 +1,17 @@
+/*
+ * uf2_version.h
+ *
+ * Created: 2024/5/8 15:16:27
+ *  Author: YHW
+ */ 
+
+
+#ifndef UF2_VERSION_H_
+#define UF2_VERSION_H_
+
+
+#define UF2_VERSION_BASE "1.0" 
+
+
+
+#endif /* UF2_VERSION_H_ */
